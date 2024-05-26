974c5299f4b1b638b80e6581262aeec682e84686
Ammar <ammynice0@gmail.com>
2024-05-26 13:27:01 +0500

    Credit reports page scrapper completed

--- app/services/credit_dyno_service.rb
+++ app/services/credit_dyno_service.rb
@@ -1,36 +1,26 @@
 # app/services/identityiq_service.rb
 class CreditDynoService
   BASE_URL = 'https://creditdyno.com/'
-  attr_reader :username, :password, :security_question, :service
 
-  def initialize(username, password, security_question, service)
-    @username = username
-    @password = password
-    @service = service
-    @security_question = security_question
-    @driver = Selenium::WebDriver.for :chrome
-    @wait = Selenium::WebDriver::Wait.new(timeout: 20)
+  def self.login(username, password)
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to "#{BASE_URL}/customer_login.asp"
+
+    driver.find_element(name: 'username').send_keys(username)
+    driver.find_element(name: 'password').send_keys(password)
+    driver.find_element(:tag_name, 'button').click
+
+    driver
   end
 
-  def fetch_credit_report
-    login
-    navigate_to_credit_report
+  def self.navigate_to_credit_reports(driver)
+    driver.navigate.to("#{BASE_URL}/cp6/mcc_creditreports_v2.asp")
+    driver.manage.timeouts.page_load = 10
+    driver
   end
 
-  private
-
-  def login
-    @driver.navigate.to("#{BASE_URL}/customer_login.asp")
-    @driver.find_element(name: 'username').send_keys(@username)
-    @driver.find_element(name: 'password').send_keys(@password)
-    @driver.find_element(:tag_name, 'button').click
-  end
-
-  def navigate_to_credit_report
-    @driver.navigate.to("#{BASE_URL}/cp6/mcc_creditreports_v2.asp")
-
-    # Get the HTML content
-    html_content = @driver.page_source
-    html_content
+  def self.get_page_html(driver)
+    element = driver.find_element(:tag_name, 'body')
+    element.attribute("outerHTML")
   end
 end
