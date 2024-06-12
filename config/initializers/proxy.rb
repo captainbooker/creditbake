67ff00a7023be95ca88024f2540ba2f6dbc8ad2a
Darren Booker <darrensoftdev+1@gmail.com>
2024-06-12 09:06:56 -0700

    update proxy

--- config/initializers/proxy.rb
+++ config/initializers/proxy.rb
@@ -1,20 +1,15 @@
-# config/initializers/proxy.rb
 require 'net/http'
 require 'uri'
 
-if ENV['QUOTAGUARDSTATIC_URL']
-  uri = URI.parse(ENV['QUOTAGUARDSTATIC_URL'])
-  proxy_class = Net::HTTP::Proxy(uri.host, uri.port, uri.user, uri.password)
-
-  Net::HTTP.class_eval do
-    @proxy_class = proxy_class
-
-    def self.new(*args, &block)
-      if @proxy_class
-        @proxy_class.new(*args, &block)
-      else
-        super
-      end
+# You don't need to redefine Net::HTTP globally. Instead, configure the proxy per request.
+def use_quotaguard_proxy(uri)
+  if ENV['QUOTAGUARDSTATIC_URL']
+    proxy_uri = URI.parse(ENV['QUOTAGUARDSTATIC_URL'])
+    Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password).start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      yield http
+    end
+  else
+    Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      yield http
     end
   end
-end
