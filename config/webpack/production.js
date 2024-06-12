91e000e4baeca04ba18cd43b06ae609eb5e6e5dd
Darren Booker <darrensoftdev+1@gmail.com>
2024-06-11 17:54:31 -0700

    add webpack config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.loaders.append('babel', {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env'],
+      plugins: [
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-private-property-in-object',
+        '@babel/plugin-proposal-class-properties'
+      ]
+    }
+  }
+})
+
 module.exports = environment.toWebpackConfig()
