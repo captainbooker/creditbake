9e4dbf6c233400cfe3c7fdf61229a546b0644716
Darren Booker <darrensoftdev+1@gmail.com>
2024-06-19 10:04:15 -0700

    error on browser for mobile

--- app/services/identityiq_service.rb
+++ app/services/identityiq_service.rb
@@ -14,8 +14,12 @@ class IdentityiqService
     # Configure Chrome options to run in headless mode
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless') # Run in headless mode
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu') # Disable GPU hardware acceleration
     options.add_argument('--window-size=1920,1080') # Set a default window size
+    options.add_argument('--disable-software-rasterizer')
+    options.add_argument('--remote-debugging-port=9222')
 
     @driver = Selenium::WebDriver.for :chrome, options: options
   end
