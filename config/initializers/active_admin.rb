795a45ef88b663ea6440762e1eb4a757061550c2
Darren Booker <darrensoftdev+1@gmail.com>
2024-06-26 08:07:17 -0700

    fix activ admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -72,7 +72,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the application controller.
-  # config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = :authenticate_admin_user!
 
   # == User Authorization
   #
@@ -109,7 +109,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
-  # config.current_user_method = :current_admin_user
+  config.current_user_method = :current_admin_user
 
   # == Logging Out
   #
@@ -127,7 +127,7 @@ ActiveAdmin.setup do |config|
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  # config.logout_link_method = :get
+  config.logout_link_method = :get
 
   # == Root
   #
