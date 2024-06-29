c6da824084743ffdbe8dcaff147629a816255c15
Darren Booker <darrensoftdev+1@gmail.com>
2024-06-28 19:47:39 -0700

    update controller

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,10 +1,8 @@
-module Admin
-  class UploadsController < ApplicationController
-    before_action :authenticate_admin_user!
+class UploadsController < ApplicationController
+  before_action :authenticate_admin_user!
 
-    def create
-      blob = ActiveStorage::Blob.create_and_upload!(io: params[:file], filename: params[:file].original_filename)
-      render json: { link: url_for(blob) }, content_type: 'text/html'
-    end
+  def create
+    blob = ActiveStorage::Blob.create_and_upload!(io: params[:file], filename: params[:file].original_filename)
+    render json: { link: url_for(blob) }, content_type: 'text/html'
   end
 end
